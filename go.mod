ab663203ecad11bae7d81c04f5d353e64ec76415
dirkarnez <smalldirkalex@gmail.com>
2024-11-17 22:17:18 +0800

    - wip

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/dirkarnez/levenshtein-search-lib
+
+go 1.21.0
+
+require (
+	github.com/agnivade/levenshtein v1.2.0
+	github.com/patrickmn/go-cache v2.1.0+incompatible
+	github.com/samber/lo v1.47.0
+)
+
+require golang.org/x/text v0.16.0 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,13 @@
+package main
+
 import (
 	"fmt"
+	"github.com/agnivade/levenshtein"
 	"github.com/patrickmn/go-cache"
+	"github.com/samber/lo"
+	// "math/rand"
+	"math"
+	"strings"
 	"time"
 )
 
@@ -11,15 +18,65 @@ type User struct {
   Age          uint8          // An unsigned 8-bit integer
 }
 
-func Levenshtein () {
-	
+func LevenshteinRatio (searchString, data string) float64 {
+	return 1.0 - (float64(levenshtein.ComputeDistance(searchString, data)) / float64(math.Max(float64(len(searchString)), float64(len(data)))))
 }
+
+func Levenshtein[T any, Slice ~[]T](searchString string, collection Slice, getters ...func(item T) string) Slice {
+	for _, getter := range getters {
+	
+	}
+
+	lo.Map(collection, func(_, index int) bool {
+		return strconv.FormatInt(x, 10)
+	})
+
+
+	for _, getter := range getters {
+		data := getter(x)
+		ratio := LevenshteinRatio(searchString, data)
+		fmt.Println("searchString", searchString, "data", data, "LevenshteinRatio", ratio)
+	}
+	
+	return lo.Filter(collection, func(x T, index int) bool {
+
+		return true
+	})
+}
+
+// func GetRandomNumber(min, max int) int {
+// 	rand.Seed(time.Now().UnixNano())
+// 	return rand.Intn(max - min) + min
+// }
+
+// func Shuffle[L interface{}](array []L) {
+// 	rand.Seed(time.Now().UnixNano())
+// 	rand.Shuffle(len(a), func(i, j int) { a[i], a[j] = a[j], a[i] })
+// }
+
+// func GetRandomName() string {
+// 	a := []string{"1213"}
+// 	Shuffle(&a)
+// 	return a[0]
+// }
+
+var names = []string{
+	"Isaac Thomson","Oliver Henderson","Bella Harris","Olivia Slater","Heather Skinner","Joe Cornish","Una Paige","Alexandra Turner","Jason Sharp","Heather Jones","Samantha Vaughan","Rebecca Sanderson","Kevin Bond","Gordon Gibson","Yvonne Scott","Dylan Brown","Simon Churchill","Irene Coleman","Peter Miller","Nicola Hughes","Fiona Forsyth","Virginia Mackay","Anne Parr","Gordon Marshall","Sophie Young","Anne Hart","Cameron James","Colin Davidson","Frank Graham","Joseph Murray","Audrey Campbell","Samantha Mackenzie","Felicity Manning","Bernadette Mackenzie","Felicity Lambert","Carl Burgess","Natalie Wilkins","Joseph Wilson","Nicholas Scott","Caroline Hill","Keith MacDonald","Andrew Payne","Dan Fisher","Blake Russell","Leonard Kelly","Stephen Bond","Victor MacDonald","Jacob Underwood","Jonathan Rees","Megan Robertson","Warren Fisher","Robert Campbell","Olivia Metcalfe","Nicola Brown","Sue Peake","Joanne Avery","Jacob Powell","Lillian Bailey","Emily Howard","Rachel Sharp","Peter Peters","Lisa Gray","Ryan Cameron","Rose Bond","Frank Taylor","Sophie Paige","Austin Lyman","Maria Ross","Victoria Thomson","Ella Murray","Andrea Davidson","Lucas Lambert","Katherine Gray","David Davies","Sarah Walker","Robert Quinn","Jessica McLean","Amy Burgess","David MacDonald","Samantha Ball","Amanda Chapman","Wanda Brown","Diana Wright","Sebastian Ogden","Audrey Wilson","Elizabeth MacLeod","Wanda Hughes","Anna Russell","Claire Cameron","Tracey Duncan","Kevin Hodges","Benjamin Gibson","Matt Piper","Theresa Paige","Austin Henderson","Michelle Brown","Anna Berry","Isaac Gill","Evan Reid","Yvonne Metcalfe",
+}
+
 func main() {
 	users := []User{}
+	for i, name := range names {
+		id := uint(i + 1)
+		email := strings.ToLower(strings.ReplaceAll(name, " ", "")) + "@gmail.com"
+		fmt.Println("id", id, "name", name, "email", email)
+		users = append(users, User{ID: id, Name: name, Email: &email})
+	}
+	
 	
 	// // Create a cache with a default expiration time of 5 minutes, and which
 	// // purges expired items every 10 minutes
-	// c := cache.New(5*time.Minute, 10*time.Minute)
+	c := cache.New(5*time.Minute, 10*time.Minute)
 
 	// // Set the value of the key "foo" to "bar", with the default expiration time
 	// c.Set("users", "bar", cache.DefaultExpiration)
@@ -29,18 +86,24 @@ func main() {
 	// c.Delete("baz")
 	c.Set("users", users, cache.NoExpiration)
 
-	// Get the string associated with the key "foo" from the cache
+	// // Get the string associated with the key "foo" from the cache
 	usersFound, found := c.Get("users")
 	if found {
+		usersFoundCasted := usersFound.([]User)
+		fmt.Println("found", len(usersFoundCasted))
 		Levenshtein(
-			usersFound.([]User), 
-			func (u *User) {
+			"olv",
+			usersFoundCasted, 
+			func (u User) string {
 				return u.Name
 			},
-			func (u *User) {
-				return u.Email
+			func (u User) string {
+				return *u.Email
 			},
 		)
+
+	} else {
+		fmt.Println("not found")
 	}
 
 	// // This gets tedious if the value is used several times in the same function.
